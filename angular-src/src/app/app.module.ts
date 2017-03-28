86cbb8d01351ecc980e3f27358c35843a4004e0c
Daniel Wildman <iwildman1@hotmail.com>
2017-03-28 17:39:53 +0100

    Completed Angular2 scaffold

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,17 +2,60 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddComponent } from './components/add/add.component';
+import { EditComponent } from './components/edit/edit.component';
+
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "dashboard",
+    component: DashboardComponent
+  },
+  {
+    path: "dashboard/new",
+    component: AddComponent
+  },
+  {
+    path: "dashboard/edit/:id",
+    component: EditComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent,
+    DashboardComponent,
+    AddComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
