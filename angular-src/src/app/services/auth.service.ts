c5a66c7d039e8ef705572e6abd79bfecc0bab56c
Daniel Wildman <iwildman1@hotmail.com>
2017-03-29 12:18:43 +0100

    Fixed localhost issues

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
     let headers = new Headers();
 
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
+    return this.http.post('users/register', user, {headers: headers})
       .map(res => res.json());
   }
 
@@ -24,7 +24,7 @@ export class AuthService {
     let headers = new Headers();
 
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
+    return this.http.post('users/authenticate', user, {headers: headers})
       .map(res => res.json());
   }
 
@@ -35,7 +35,7 @@ export class AuthService {
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type', 'application/json');
 
-    return this.http.get('http://localhost:3000/users/settings', {headers: headers})
+    return this.http.get('users/settings', {headers: headers})
       .map(res => res.json());
   }
 
